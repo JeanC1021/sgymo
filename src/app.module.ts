4a9e5bfcd40e954fc233e3703c6584139e2e46dc
Jean <jeancardona21@gmail.com>
2024-06-22 19:30:52 -0500

    create auth and tasks folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,8 +16,7 @@ import { User } from './user/entities/user.entity';
       database: 'sgym',
       entities: [User],
       synchronize: true,
-    }),],
-  controllers: [AppController],
-  providers: [AppService],
+    }),
+    AuthModule,],
 })
 export class AppModule { }
